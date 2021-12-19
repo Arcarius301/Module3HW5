24131da88e961051bba7184a77b3370ca6ac0a37
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2021-12-19 20:24:58 +0200

    Done.

---
+++ Module3HW5/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace Module3HW5
+{
+    public class Class1
+    {
+        public async Task<string> ReadHelloAsync()
+        {
+            return await File.ReadAllTextAsync("hello.txt");
+        }
+
+        public async Task<string> ReadWorldAsync()
+        {
+            return await File.ReadAllTextAsync("world.txt");
+        }
+
+        public async Task<string> GetResult()
+        {
+            var string1 = await ReadHelloAsync();
+            var string2 = await ReadWorldAsync();
+
+            return $"{string1} {string2}!";
+        }
+    }
+}
--- Module3HW5/Program.cs
+++ Module3HW5/Program.cs
@@ -6,6 +6,9 @@ namespace Module3HW5
     {
         public static void Main(string[] args)
         {
+            var class1 = new Class1();
+            Console.WriteLine(class1.GetResult().Result);
+            Console.ReadKey();
         }
     }
 }
